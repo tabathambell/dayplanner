4c60e1d9134c5a4687349b8ff8c7a57249b591c3
Tabatha Bell <tabathambell@live.com>
2020-11-12 22:28:51 -0500

    finished moment with the date and time and then organized the colors using moment

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,22 @@
-$(".description").each(function(index, element) {
-    $(this).addClass("present");
-})
+$(document).ready(function() {
+
+    var today = moment().format("MMMM Do YYYY");
+    var currentHour = moment().format("H");
+
+    $("#currentDay").text(today);
+
+    $(".description").each(function(index, element) {
+        var hour = index + 9;
+        if (hour == currentHour) {
+            $(this).addClass("present");
+        } else if (hour > currentHour) {
+            $(this).addClass("future");
+        } else (hour < currentHour);
+            $(this).addClass("past");
+    });
+
+    $(".saveBtn").click(function() {
+        
+    });
+    
+});
